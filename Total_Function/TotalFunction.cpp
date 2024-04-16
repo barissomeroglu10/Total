6af41cc36dc81f4842be4ec95c21b3bbefe5c3a2
baris <baris@BARIS_SOMEROGLU>
2024-04-16 13:36:08 +0300

    Add project files.

---
+++ Total_Function/TotalFunction.cpp
@@ -0,0 +1,49 @@
+﻿/*
+In this code, we will calculate a total from 1 to Number. We get number from user.
+
+Developer: Barış Someroğlu
+Date: 16.04.2024 - 01:30 pm
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+// Prtototype for functions
+void Result(int);
+int TotalOfNumber(int);
+
+// Globale variable
+int Total = 0;
+
+int main()
+{
+	int Number;
+	
+	cout << "Please Enter a Number = ";
+	cin >> Number;
+
+	// Call the function
+	Result(Number);
+
+
+	return 0;
+}
+
+// Function defenation
+void Result(int Number)
+{
+	cout << "Result is = " << TotalOfNumber(Number) << endl;
+}
+
+// Function defenation
+int TotalOfNumber(int Number)
+{
+	for (int i = 1; i <= Number; i++)
+	{
+		Total += i;
+	}
+	
+	return Total;
+}
